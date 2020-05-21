8b7f6ba2c97ae92ae6c4a03cf6ae47e2a7414ab8
KJnong <57115252+KJnong@users.noreply.github.com>
2020-05-21 23:21:53 +0200

    clearing input after submision

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -11,13 +11,14 @@ class TodoForm extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         this.props.formInfo(this.state)
+        this.setState({content:""})
     }
 
     render() {
         return (
             <div class={"row", "container"}>
                 <form onSubmit={this.onSubmit} class="input-field col s6">
-                    <input onChange={this.changeHandler} id="todo" placeholder='Type here' type="text" class="validate" />
+                    <input onChange={this.changeHandler} id="todo" value={this.state.content} placeholder='Type here' type="text" class="validate" />
                     <label className="active" for="first_name2">Add New Task</label>
                 </form>
             </div>
