3c77d011f2cf7e07ce89bf3d520a36273bfbd5fa
KJnong <57115252+KJnong@users.noreply.github.com>
2020-05-21 19:44:17 +0200

    added placeholder for form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -16,7 +16,7 @@ class TodoForm extends Component {
         return (
             <div class={"row", "container"}>
                 <form onSubmit={this.onSubmit} class="input-field col s6">
-                    <input onChange={this.changeHandler} id="todo" type="text" class="validate" />
+                    <input onChange={this.changeHandler} id="todo" placeholder='Type here' type="text" class="validate" />
                     <label class="active" for="first_name2">Add New Task</label>
                 </form>
             </div>
